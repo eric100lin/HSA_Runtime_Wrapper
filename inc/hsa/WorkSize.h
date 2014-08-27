d1e864cd0aea52f0bf7b17c1b9ad38dc2716e54e
Tzu Hsiang Lin <thlin@pllab.cs.nthu.edu.tw>
2014-08-27 14:38:42 +0800

    Correct the WorkSize assignment fault.

--- inc/hsa/WorkSize.h
+++ inc/hsa/WorkSize.h
@@ -37,8 +37,8 @@ namespace hsa
 			static size_t LargestPow2Size(const size_t size);
 			static size_t DivRoundUp(const size_t dividend, const size_t divisor);
 
-			WorkSize(const size_t _s0) : d(1) { s[0] = _s0, s[1] = 0, s[2] = 0; }
-			WorkSize(const size_t _s0, const size_t _s1) : d(2) { s[0] = _s0, s[1] = _s1, s[2] = 0; }
+			WorkSize(const size_t _s0) : d(1) { s[0] = _s0, s[1] = 1, s[2] = 1; }
+			WorkSize(const size_t _s0, const size_t _s1) : d(2) { s[0] = _s0, s[1] = _s1, s[2] = 1; }
 			WorkSize(const size_t _s0, const size_t _s1, const size_t _s2) : d(3) { s[0] = _s0, s[1] = _s1, s[2] = _s2; }
 			WorkSize(const unsigned int _d, const size_t _s0, const size_t _s1, const size_t _s2) : d(_d) { s[0] = _s0, s[1] = _s1, s[2] = _s2; }
 			WorkSize(const WorkSize &_workSize) : d(_workSize.Dimensions()) { s[0] = _workSize.Size0(); s[1] = _workSize.Size1(); s[2] = _workSize.Size2(); }
